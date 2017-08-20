af00940dd529ef3345dc4ec45f1149bdd274f7d8
Hannes <Hannes.Holzwarth@me.com>
2017-08-20 19:59:37 +0200

    Code to set up the http server

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict';
 
 const http = require('http');
+const express = require('express');
+
+const router = express();
+const server = http.createServer(router);
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const auth = require('./lib/auth');
+
+router.use(cors());
+router.use(bodyParser.json());
+
+router.use(require('./lib/response-helper'));
+
+router.post('/login', auth.login);
+router.post('/register', auth.register, auth.login);
+
+router.use(auth.protect);
+
+router.post('/diff', require('./lib/routes/diff'));
+
+server.listen(80, '0.0.0.0', () => console.log('Listening on port 80'));
